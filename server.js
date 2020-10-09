84a219d9e500331dd017e1f3470e3481f435c922
tylerbertmore <tylercsmith94@gmail.com>
2020-10-10 04:28:25 +0700

    Adds partials, sets view engine, add fake morgan middleware

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
+//-------------------------------------------- MODULES
 const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
 
+//-------------------------------------------- VIEW ENGINE
+app.set('view engine', 'ejs')
+
+
+//-------------------------------------------- MIDDLEWARE
+
+// MORGAN REPLACEMENT
+app.use((req, res, next) => {
+    const method = req.method;
+    const path = req.url;
+    const timestamp = new Date().toLocaleTimeString();
+    console.log(`${method} ${path} ${timestamp}`);
+    next(); // Allow the request to move on to the next middleware in the chain
+  });
+//-------------------------------------------- ROUTES
 app.get('/', (req, res) => {
-    console.log('terminal hellloooo');
-    res.send('helllooooo')
+    res.render('index');
 })
 
+
+
+//-------------------------------------------- LISTENER
 app.listen(PORT, () => {
     console.log(`I'm listening on port ${PORT}`)
 })
