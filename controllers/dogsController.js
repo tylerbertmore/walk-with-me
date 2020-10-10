55672b946651305e2ecaae706ee8c2d6bccca684
jedgodsey <jed.godsey@gmail.com>
2020-10-09 21:37:52 -0700

    added showDog page and route, seeded dog collection

--- controllers/dogsController.js
+++ controllers/dogsController.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
+//--------------------------------- SEED DOGS
+const firstDogs = require('../models/dogs.js');
+
+db.Dog.insertMany(firstDogs, (err, newDogs) => {
+    err ? console.log(err) : console.log(newDogs);
+})
 
 //-------------------------------------------- ROUTES
+// paths assume '/dogs'
+
 router.get('/', (req,res) => {
     res.send('dogs index');
+})
+
+router.get('/:dogId', (req, res) => {
+    db.Dog.findById(req.params.dogId, (err, foundDog) => {
+        err ? console.log(err) : res.render('dogs/show', {dog: foundDog})
+    })
 })
 
 module.exports = router
---
+++ models/dogs.js
@@ -0,0 +1,151 @@
+const dogs = [
+    {
+        name: 'Rex',
+        breed: 'Beagle',
+        birthDate: '12/12/2020',
+        sex: 'Male',
+        img: './resources/dogpics/beagle.jpg'
+    },
+    {
+        name: 'Monica',
+        breed: 'Black Labrador Retriever',
+        birthDate: '12/12/2020',
+        sex: 'Female',
+        img: './resources/dogpics/blacklab.jpg'
+    },
+    {
+        name: 'Fido',
+        breed: 'Bloodhound',
+        birthDate: '12/12/2020',
+        sex: 'Male',
+        img: './resources/dogpics/bloodhound.jpg'
+    },
+    {
+        name: 'Bailey',
+        breed: 'Bouvier des Flandres',
+        birthDate: '12/12/2020',
+        sex: 'Female',
+        img: './resources/dogpics/bouvier.jpg'
+    },
+    {
+        name: 'Charlie',
+        breed: 'Boxer',
+        birthDate: '12/12/2020',
+        sex: 'Male',
+        img: './resources/dogpics/boxer.jpg'
+    },
+    {
+        name: 'Amethyst',
+        breed: 'English Bulldog',
+        birthDate: '12/12/2020',
+        sex: 'Female',
+        img: './resources/dogpics/bulldog.jpg'
+    },
+    {
+        name: 'Madonna',
+        breed: 'Chihuahua',
+        birthDate: '12/12/2020',
+        sex: 'Female',
+        img: './resources/dogpics/chihuahua.jpg'
+    },
+    {
+        name: 'John',
+        breed: 'French Bulldog',
+        birthDate: '12/12/2020',
+        sex: 'Male',
+        img: './resources/dogpics/frenchbulldog.jpg'
+    },
+    {
+        name: 'She-Rah',
+        breed: 'German Shepherd',
+        birthDate: '12/12/2020',
+        sex: 'Female',
+        img: './resources/dogpics/germanshepherd.jpg'
+    },
+    {
+        name: 'Arbuckle',
+        breed: 'Golden Retriever',
+        birthDate: '12/12/2020',
+        sex: 'Male',
+        img: './resources/dogpics/goldenretriever.jpg'
+    },
+    {
+        name: 'Jane',
+        breed: 'Great Dane',
+        birthDate: '12/12/2020',
+        sex: 'Female',
+        img: './resources/dogpics/greatdane.jpg'
+    },
+    {
+        name: 'Chester',
+        breed: 'Siberian Husky',
+        birthDate: '12/12/2020',
+        sex: 'Male',
+        img: './resources/dogpics/husky.jpg'
+    },
+    {
+        name: 'Montana',
+        breed: 'Jack Russell Terrier',
+        birthDate: '12/12/2020',
+        sex: 'Female',
+        img: './resources/dogpics/jackrussell.jpg'
+    },
+    {
+        name: 'Albert',
+        breed: 'Labradoodle',
+        birthDate: '12/12/2020',
+        sex: 'Male',
+        img: './resources/dogpics/labradoodle.jpg'
+    },
+    {
+        name: 'Dexy',
+        breed: 'Poodle',
+        birthDate: '12/12/2020',
+        sex: 'Female',
+        img: './resources/dogpics/poodle.jpg'
+    },
+    {
+        name: 'Kazoo',
+        breed: 'Pug',
+        birthDate: '12/12/2020',
+        sex: 'Male',
+        img: './resources/dogpics/pug.jpg'
+    },
+    {
+        name: 'Bernadette',
+        breed: 'Rhodesian Ridgeback',
+        birthDate: '12/12/2020',
+        sex: 'Female',
+        img: './resources/dogpics/rhodesian.jpg'
+    },
+    {
+        name: 'Hemingway',
+        breed: 'Schipperke',
+        birthDate: '12/12/2020',
+        sex: 'Male',
+        img: './resources/dogpics/schipperke.jpg'
+    },
+    {
+        name: 'Sally',
+        breed: 'Spaniel',
+        birthDate: '12/12/2020',
+        sex: 'Female',
+        img: './resources/dogpics/spaniel.jpg'
+    },
+    {
+        name: 'Turk',
+        breed: 'Soft-Coated Wheaten Terrier',
+        birthDate: '12/12/2020',
+        sex: 'Male',
+        img: './resources/dogpics/wheaten.jpg'
+    },
+    {
+        name: 'Thesba',
+        breed: 'Yellow Labrodor Retriever',
+        birthDate: '12/12/2020',
+        sex: 'Female',
+        img: './resources/dogpics/yellowlab.jpg'
+    }
+]
+
+module.exports = dogs;
