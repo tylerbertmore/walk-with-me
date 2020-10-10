3c7f34b87103c54f66cae0b6fedc227f45c4312c
jedgodsey <jed.godsey@gmail.com>
2020-10-09 23:14:33 -0700

    finished user edit page. made matching get route and put route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
 
 //-------------------------------------------- ROUTES
@@ -7,6 +8,27 @@ const router = express.Router();
 
 router.get('/', (req,res) => {
     res.send('users index');
+})
+
+router.get('/:user', (req, res) => {
+    db.User.findById(req.params.user, (err, foundUser) => {
+        err ? console.log(err) : res.render('users/show', {user: foundUser})
+    })
+})
+
+router.get('/:user/edit', (req, res) => {
+    db.User.findById(req.params.user, (err, foundUser) => {
+        err ? console.log(err) : res.render('users/edit', {user: foundUser})
+    })
+})
+
+router.put('/:user', (req, res) => {
+    db.User.findByIdAndUpdate(req.params.user,
+        req.body,
+        {new: true},
+        (err, updated) => {
+            err ? console.log(err) : res.redirect(`${updated._id}`)
+    })
 })
 
 module.exports = router
