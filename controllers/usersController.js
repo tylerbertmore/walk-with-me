d51b580bfb0d74eae47bd587295e5a5fc6b08f25
jedgodsey <jed.godsey@gmail.com>
2020-10-10 22:06:01 -0700

    added delete function and user dashboard route/page

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,28 +6,42 @@ const db = require('../models');
 //-------------------------------------------- ROUTES
 // all routes assume '/users'
 
-router.get('/', (req,res) => {
-    res.send('users index');
+// render dashboard upon logging in
+router.get('/:user', (req,res) => {
+    db.User.findById(req.params.user, (err, foundUser) => {
+        // add appointments query
+        err ? console.log(err) : res.render('users/dashboard', {user: foundUser})
+    })
 })
 
-router.get('/:user', (req, res) => {
+// render page with just user info
+router.get('/:user/show', (req, res) => {
     db.User.findById(req.params.user, (err, foundUser) => {
         err ? console.log(err) : res.render('users/show', {user: foundUser})
     })
 })
 
+// render page to edit or delete account
 router.get('/:user/edit', (req, res) => {
     db.User.findById(req.params.user, (err, foundUser) => {
         err ? console.log(err) : res.render('users/edit', {user: foundUser})
     })
 })
 
+// update account info in database
 router.put('/:user', (req, res) => {
     db.User.findByIdAndUpdate(req.params.user,
         req.body,
         {new: true},
         (err, updated) => {
             err ? console.log(err) : res.redirect(`${updated._id}`)
+    })
+})
+
+// delete account from database
+router.delete('/:user', (req, res) => {
+    db.User.findByIdAndDelete(req.params.user, (err, deleted) => {
+        err ? console.log(err) : res.redirect('../')
     })
 })
 
