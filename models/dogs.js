098bb380106a9ca1ac2559c0f8b43c8bdc9e3a47
jedgodsey <jed.godsey@gmail.com>
2020-10-12 23:42:38 -0700

    fixed static rendering and added minimal css styling

--- models/dogs.js
+++ models/dogs.js
@@ -4,147 +4,147 @@ const dogs = [
         breed: 'Beagle',
         birthDate: '12/12/2018',
         sex: 'Male',
-        img: './public/images/dogpics/beagle.jpg'
+        img: '/images/dogpics/beagle.jpg'
     },
     {
         name: 'Monica',
         breed: 'Black Labrador Retriever',
         birthDate: '12/12/2010',
         sex: 'Female',
-        img: './public/images/dogpics/blacklab.jpg'
+        img: '/images/dogpics/blacklab.jpg'
     },
     {
         name: 'Fido',
         breed: 'Bloodhound',
         birthDate: '12/12/2016',
         sex: 'Male',
-        img: './public/images/dogpics/bloodhound.jpg'
+        img: '/images/dogpics/bloodhound.jpg'
     },
     {
         name: 'Bailey',
         breed: 'Bouvier des Flandres',
         birthDate: '12/12/2009',
         sex: 'Female',
-        img: './public/images/dogpics/bouvier.jpg'
+        img: '/images/dogpics/bouvier.jpg'
     },
     {
         name: 'Charlie',
         breed: 'Boxer',
         birthDate: '12/12/2019',
         sex: 'Male',
-        img: './public/images/dogpics/boxer.jpg'
+        img: '/images/dogpics/boxer.jpg'
     },
     {
         name: 'Amethyst',
         breed: 'English Bulldog',
         birthDate: '12/12/2014',
         sex: 'Female',
-        img: './public/images/dogpics/bulldog.jpg'
+        img: '/images/dogpics/bulldog.jpg'
     },
     {
         name: 'Madonna',
         breed: 'Chihuahua',
         birthDate: '12/12/2014',
         sex: 'Female',
-        img: './public/images/dogpics/chihuahua.jpg'
+        img: '/images/dogpics/chihuahua.jpg'
     },
     {
         name: 'John',
         breed: 'French Bulldog',
         birthDate: '12/12/2014',
         sex: 'Male',
-        img: './public/images/dogpics/frenchbulldog.jpg'
+        img: '/images/dogpics/frenchbulldog.jpg'
     },
     {
         name: 'She-Rah',
         breed: 'German Shepherd',
         birthDate: '12/12/2012',
         sex: 'Female',
-        img: './public/images/dogpics/germanshepherd.jpg'
+        img: '/images/dogpics/germanshepherd.jpg'
     },
     {
         name: 'Arbuckle',
         breed: 'Golden Retriever',
         birthDate: '12/12/2015',
         sex: 'Male',
-        img: './public/images/dogpics/goldenretriever.jpg'
+        img: '/images/dogpics/goldenretriever.jpg'
     },
     {
         name: 'Jane',
         breed: 'Great Dane',
         birthDate: '12/12/2016',
         sex: 'Female',
-        img: './public/images/dogpics/greatdane.jpg'
+        img: '/images/dogpics/greatdane.jpg'
     },
     {
         name: 'Chester',
         breed: 'Siberian Husky',
         birthDate: '12/12/2017',
         sex: 'Male',
-        img: './public/images/dogpics/husky.jpg'
+        img: '/images/dogpics/husky.jpg'
     },
     {
         name: 'Montana',
         breed: 'Jack Russell Terrier',
         birthDate: '12/12/2019',
         sex: 'Female',
-        img: './public/images/dogpics/jackrussell.jpg'
+        img: '/images/dogpics/jackrussell.jpg'
     },
     {
         name: 'Albert',
         breed: 'Labradoodle',
         birthDate: '12/12/2005',
         sex: 'Male',
-        img: './public/images/dogpics/labradoodle.jpg'
+        img: '/images/dogpics/labradoodle.jpg'
     },
     {
         name: 'Dexy',
         breed: 'Poodle',
         birthDate: '12/12/2006',
         sex: 'Female',
-        img: './public/images/dogpics/poodle.jpg'
+        img: '/images/dogpics/poodle.jpg'
     },
     {
         name: 'Kazoo',
         breed: 'Pug',
         birthDate: '12/12/2007',
         sex: 'Male',
-        img: './public/images/dogpics/pug.jpg'
+        img: '/images/dogpics/pug.jpg'
     },
     {
         name: 'Bernadette',
         breed: 'Rhodesian Ridgeback',
         birthDate: '12/12/2008',
         sex: 'Female',
-        img: './public/images/dogpics/rhodesian.jpg'
+        img: '/images/dogpics/rhodesian.jpg'
     },
     {
         name: 'Hemingway',
         breed: 'Schipperke',
         birthDate: '12/12/2009',
         sex: 'Male',
-        img: './public/images/dogpics/schipperke.jpg'
+        img: '/images/dogpics/schipperke.jpg'
     },
     {
         name: 'Sally',
         breed: 'Spaniel',
         birthDate: '12/12/2010',
         sex: 'Female',
-        img: './public/images/dogpics/spaniel.jpg'
+        img: '/images/dogpics/spaniel.jpg'
     },
     {
         name: 'Turk',
         breed: 'Soft-Coated Wheaten Terrier',
         birthDate: '12/12/2011',
         sex: 'Male',
-        img: './public/images/dogpics/wheaten.jpg'
+        img: '/images/dogpics/wheaten.jpg'
     },
     {
         name: 'Thesba',
         breed: 'Yellow Labrodor Retriever',
         birthDate: '12/12/2012',
         sex: 'Female',
-        img: './public/images/dogpics/yellowlab.jpg'
+        img: '/images/dogpics/yellowlab.jpg'
     }
 ]
 
