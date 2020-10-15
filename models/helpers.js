c933fac2c405230722fa340698d416d45c9209ed
jedgodsey <jed.godsey@gmail.com>
2020-10-15 12:35:26 -0700

    dasbhoard tiles rendering in order and links working

--- models/helpers.js
+++ models/helpers.js
@@ -1,56 +1,57 @@
 let format = array => {
-    switch (array[2]) {
-        case 0:
-            array[2] = 'Monday';
-        break;
-        case 1:
-            array[2] = 'Tuesday';
-        break;
-        case 2:
-            array[2] = 'Wednesday';
-        break;
-        case 3:
-            array[2] = 'Thursday';
-        break;
-        case 4:
-            array[2] = 'Friday';
-        break;
-        case 5:
-            array[2] = 'Saturday';
-        break;
-        case 6:
-            array[2] = 'Sunday';
-        default:
-            array[2] = 'Today';
-    }
-
     switch (array[1]) {
         case 0:
-            array[1] = '9:00 AM';
+            array[1] = 'Monday';
         break;
         case 1:
-            array[1] = '10:00 AM';
+            array[1] = 'Tuesday';
         break;
         case 2:
-            array[1] = '11:00 AM';
+            array[1] = 'Wednesday';
         break;
         case 3:
-            array[1] = '12:00 PM';
+            array[1] = 'Thursday';
         break;
         case 4:
-            array[1] = '1:00 PM';
+            array[1] = 'Friday';
         break;
         case 5:
-            array[1] = '2:00 PM';
+            array[1] = 'Saturday';
         break;
         case 6:
-            array[1] = '3:00 PM';
-        break;
-        case 7:
-            array[1] = '4:00 PM';
+            array[1] = 'Sunday';
         break;
         default:
-            array[1] = 'now';
+            array[1] = 'Today';
+    }
+
+    switch (array[2]) {
+        case 0:
+            array[2] = '9:00 AM';
+        break;
+        case 1:
+            array[2] = '10:00 AM';
+        break;
+        case 2:
+            array[2] = '11:00 AM';
+        break;
+        case 3:
+            array[2] = '12:00 PM';
+        break;
+        case 4:
+            array[2] = '1:00 PM';
+        break;
+        case 5:
+            array[2] = '2:00 PM';
+        break;
+        case 6:
+            array[2] = '3:00 PM';
+        break;
+        case 7:
+            array[2] = '4:00 PM';
+        break;
+        default:
+            array[2] = 'now';
     }
     return console.log(array);
 }
