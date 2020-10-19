a07338c1ee3e04d54a7abac2c11dc392d24fb267
jedgodsey <jed.godsey@gmail.com>
2020-10-18 21:31:41 -0700

    made final changes

--- models/helpers.js
+++ models/helpers.js
@@ -1,21 +1,34 @@
 const db = require('../models');
 const cron = require('node-cron');
 
+let today = 0;
+
+cron.schedule("* 17 * * *" , function(){
+    today++
+    adjust();
+    rollOver();
+    console.log('weektest ', week)
+});
+
+let week;
+
 let rollOver = () => {
     db.Dog.updateMany({},
         {$push: {schedule: { $each: [,,,,,,,,], $slice: -56}}},
         { multi: true },
         (err, renewedDogs) => {
-            console.log(err);
-        })
+            if (err) return console.log(err);
+            db.User.updateMany({},
+                {$push: {schedule: { $each: [,,,,,,,,], $slice: -56}}},
+                { multi: true },
+                (err, renewedUsers) => {
+                    if (err) return console.log(err);
+            })
+    })
 }
 
-cron.schedule("40 56 8 * * * " , function(){
-    rollOver();
-    console.log('toad')
-});
-
 let format = array => {
+    array[1] = (array[1] % today)
     switch (array[1]) {
         case 0:
             array[1] = 'Monday';
@@ -70,11 +83,43 @@ let format = array => {
         default:
             array[2] = 'now';
     }
-    return console.log(array);
 }
 
-let logger = entry => {
-    console.log(entry);
+let adjust = () => {
+    week = [6,0,1,2,3,4,5]
+    week.forEach((item, index) => {
+       week[index] = (item + today) % 7
+    })
+    week.forEach((item, index) => {
+        switch (item) {
+            case 0:
+                week[index] = 'Monday';
+            break;
+            case 1:
+                week[index] = 'Tuesday';
+            break;
+            case 2:
+                week[index] = 'Wednesday';
+            break;
+            case 3:
+                week[index] = 'Thursday';
+            break;
+            case 4:
+                week[index] = 'Friday';
+            break;
+            case 5:
+                week[index] = 'Saturday';
+            break;
+            case 6:
+                week[index] = 'Sunday';
+            break;
+            default:
+                week[index] = 'Today';
+        }
+    })
+    return week;
 }
 
-module.exports = {format, logger};
+adjust();
+
+module.exports = {format, adjust, week};
